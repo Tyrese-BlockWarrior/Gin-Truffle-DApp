7f74542eb248886f0bb67eb1bac36e6806f63be1
TyreseLian <tyreselian3915@gmail.com>
2023-05-20 19:51:03 +0530

    binding_created

---
+++ Cert.go
@@ -0,0 +1,403 @@
+// Code generated - DO NOT EDIT.
+// This file is a generated binding and any manual changes will be lost.
+
+package main
+
+import (
+	"errors"
+	"math/big"
+	"strings"
+
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
+)
+
+// Reference imports to suppress errors if they are not otherwise used.
+var (
+	_ = errors.New
+	_ = big.NewInt
+	_ = strings.NewReader
+	_ = ethereum.NotFound
+	_ = bind.Bind
+	_ = common.Big1
+	_ = types.BloomLookup
+	_ = event.NewSubscription
+	_ = abi.ConvertType
+)
+
+// CertMetaData contains all meta data concerning the Cert contract.
+var CertMetaData = &bind.MetaData{
+	ABI: "[{\"inputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"constructor\",\"signature\":\"constructor\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"string\",\"name\":\"course\",\"type\":\"string\"},{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"id\",\"type\":\"uint256\"},{\"indexed\":false,\"internalType\":\"string\",\"name\":\"grade\",\"type\":\"string\"}],\"name\":\"Issued\",\"type\":\"event\",\"signature\":\"0xc7fc4858662996e8fe091a9941384ab745bfc51199d7c0a5e45a791ff945993c\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"name\":\"Certificates\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"name\",\"type\":\"string\"},{\"internalType\":\"string\",\"name\":\"course\",\"type\":\"string\"},{\"internalType\":\"string\",\"name\":\"grade\",\"type\":\"string\"},{\"internalType\":\"string\",\"name\":\"date\",\"type\":\"string\"}],\"stateMutability\":\"view\",\"type\":\"function\",\"constant\":true,\"signature\":\"0x9622c836\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"_id\",\"type\":\"uint256\"},{\"internalType\":\"string\",\"name\":\"_name\",\"type\":\"string\"},{\"internalType\":\"string\",\"name\":\"_course\",\"type\":\"string\"},{\"internalType\":\"string\",\"name\":\"_grade\",\"type\":\"string\"},{\"internalType\":\"string\",\"name\":\"_date\",\"type\":\"string\"}],\"name\":\"issue\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\",\"signature\":\"0xd1e7be26\"}]",
+}
+
+// CertABI is the input ABI used to generate the binding from.
+// Deprecated: Use CertMetaData.ABI instead.
+var CertABI = CertMetaData.ABI
+
+// Cert is an auto generated Go binding around an Ethereum contract.
+type Cert struct {
+	CertCaller     // Read-only binding to the contract
+	CertTransactor // Write-only binding to the contract
+	CertFilterer   // Log filterer for contract events
+}
+
+// CertCaller is an auto generated read-only Go binding around an Ethereum contract.
+type CertCaller struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// CertTransactor is an auto generated write-only Go binding around an Ethereum contract.
+type CertTransactor struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// CertFilterer is an auto generated log filtering Go binding around an Ethereum contract events.
+type CertFilterer struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// CertSession is an auto generated Go binding around an Ethereum contract,
+// with pre-set call and transact options.
+type CertSession struct {
+	Contract     *Cert             // Generic contract binding to set the session for
+	CallOpts     bind.CallOpts     // Call options to use throughout this session
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// CertCallerSession is an auto generated read-only Go binding around an Ethereum contract,
+// with pre-set call options.
+type CertCallerSession struct {
+	Contract *CertCaller   // Generic contract caller binding to set the session for
+	CallOpts bind.CallOpts // Call options to use throughout this session
+}
+
+// CertTransactorSession is an auto generated write-only Go binding around an Ethereum contract,
+// with pre-set transact options.
+type CertTransactorSession struct {
+	Contract     *CertTransactor   // Generic contract transactor binding to set the session for
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// CertRaw is an auto generated low-level Go binding around an Ethereum contract.
+type CertRaw struct {
+	Contract *Cert // Generic contract binding to access the raw methods on
+}
+
+// CertCallerRaw is an auto generated low-level read-only Go binding around an Ethereum contract.
+type CertCallerRaw struct {
+	Contract *CertCaller // Generic read-only contract binding to access the raw methods on
+}
+
+// CertTransactorRaw is an auto generated low-level write-only Go binding around an Ethereum contract.
+type CertTransactorRaw struct {
+	Contract *CertTransactor // Generic write-only contract binding to access the raw methods on
+}
+
+// NewCert creates a new instance of Cert, bound to a specific deployed contract.
+func NewCert(address common.Address, backend bind.ContractBackend) (*Cert, error) {
+	contract, err := bindCert(address, backend, backend, backend)
+	if err != nil {
+		return nil, err
+	}
+	return &Cert{CertCaller: CertCaller{contract: contract}, CertTransactor: CertTransactor{contract: contract}, CertFilterer: CertFilterer{contract: contract}}, nil
+}
+
+// NewCertCaller creates a new read-only instance of Cert, bound to a specific deployed contract.
+func NewCertCaller(address common.Address, caller bind.ContractCaller) (*CertCaller, error) {
+	contract, err := bindCert(address, caller, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &CertCaller{contract: contract}, nil
+}
+
+// NewCertTransactor creates a new write-only instance of Cert, bound to a specific deployed contract.
+func NewCertTransactor(address common.Address, transactor bind.ContractTransactor) (*CertTransactor, error) {
+	contract, err := bindCert(address, nil, transactor, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &CertTransactor{contract: contract}, nil
+}
+
+// NewCertFilterer creates a new log filterer instance of Cert, bound to a specific deployed contract.
+func NewCertFilterer(address common.Address, filterer bind.ContractFilterer) (*CertFilterer, error) {
+	contract, err := bindCert(address, nil, nil, filterer)
+	if err != nil {
+		return nil, err
+	}
+	return &CertFilterer{contract: contract}, nil
+}
+
+// bindCert binds a generic wrapper to an already deployed contract.
+func bindCert(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
+	parsed, err := CertMetaData.GetAbi()
+	if err != nil {
+		return nil, err
+	}
+	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Cert *CertRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
+	return _Cert.Contract.CertCaller.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Cert *CertRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Cert.Contract.CertTransactor.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Cert *CertRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Cert.Contract.CertTransactor.contract.Transact(opts, method, params...)
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Cert *CertCallerRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
+	return _Cert.Contract.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Cert *CertTransactorRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Cert.Contract.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Cert *CertTransactorRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Cert.Contract.contract.Transact(opts, method, params...)
+}
+
+// Certificates is a free data retrieval call binding the contract method 0x9622c836.
+//
+// Solidity: function Certificates(uint256 ) view returns(string name, string course, string grade, string date)
+func (_Cert *CertCaller) Certificates(opts *bind.CallOpts, arg0 *big.Int) (struct {
+	Name   string
+	Course string
+	Grade  string
+	Date   string
+}, error) {
+	var out []interface{}
+	err := _Cert.contract.Call(opts, &out, "Certificates", arg0)
+
+	outstruct := new(struct {
+		Name   string
+		Course string
+		Grade  string
+		Date   string
+	})
+	if err != nil {
+		return *outstruct, err
+	}
+
+	outstruct.Name = *abi.ConvertType(out[0], new(string)).(*string)
+	outstruct.Course = *abi.ConvertType(out[1], new(string)).(*string)
+	outstruct.Grade = *abi.ConvertType(out[2], new(string)).(*string)
+	outstruct.Date = *abi.ConvertType(out[3], new(string)).(*string)
+
+	return *outstruct, err
+
+}
+
+// Certificates is a free data retrieval call binding the contract method 0x9622c836.
+//
+// Solidity: function Certificates(uint256 ) view returns(string name, string course, string grade, string date)
+func (_Cert *CertSession) Certificates(arg0 *big.Int) (struct {
+	Name   string
+	Course string
+	Grade  string
+	Date   string
+}, error) {
+	return _Cert.Contract.Certificates(&_Cert.CallOpts, arg0)
+}
+
+// Certificates is a free data retrieval call binding the contract method 0x9622c836.
+//
+// Solidity: function Certificates(uint256 ) view returns(string name, string course, string grade, string date)
+func (_Cert *CertCallerSession) Certificates(arg0 *big.Int) (struct {
+	Name   string
+	Course string
+	Grade  string
+	Date   string
+}, error) {
+	return _Cert.Contract.Certificates(&_Cert.CallOpts, arg0)
+}
+
+// Issue is a paid mutator transaction binding the contract method 0xd1e7be26.
+//
+// Solidity: function issue(uint256 _id, string _name, string _course, string _grade, string _date) returns()
+func (_Cert *CertTransactor) Issue(opts *bind.TransactOpts, _id *big.Int, _name string, _course string, _grade string, _date string) (*types.Transaction, error) {
+	return _Cert.contract.Transact(opts, "issue", _id, _name, _course, _grade, _date)
+}
+
+// Issue is a paid mutator transaction binding the contract method 0xd1e7be26.
+//
+// Solidity: function issue(uint256 _id, string _name, string _course, string _grade, string _date) returns()
+func (_Cert *CertSession) Issue(_id *big.Int, _name string, _course string, _grade string, _date string) (*types.Transaction, error) {
+	return _Cert.Contract.Issue(&_Cert.TransactOpts, _id, _name, _course, _grade, _date)
+}
+
+// Issue is a paid mutator transaction binding the contract method 0xd1e7be26.
+//
+// Solidity: function issue(uint256 _id, string _name, string _course, string _grade, string _date) returns()
+func (_Cert *CertTransactorSession) Issue(_id *big.Int, _name string, _course string, _grade string, _date string) (*types.Transaction, error) {
+	return _Cert.Contract.Issue(&_Cert.TransactOpts, _id, _name, _course, _grade, _date)
+}
+
+// CertIssuedIterator is returned from FilterIssued and is used to iterate over the raw logs and unpacked data for Issued events raised by the Cert contract.
+type CertIssuedIterator struct {
+	Event *CertIssued // Event containing the contract specifics and raw log
+
+	contract *bind.BoundContract // Generic contract to use for unpacking event data
+	event    string              // Event name to use for unpacking event data
+
+	logs chan types.Log        // Log channel receiving the found contract events
+	sub  ethereum.Subscription // Subscription for errors, completion and termination
+	done bool                  // Whether the subscription completed delivering logs
+	fail error                 // Occurred error to stop iteration
+}
+
+// Next advances the iterator to the subsequent event, returning whether there
+// are any more events found. In case of a retrieval or parsing error, false is
+// returned and Error() can be queried for the exact failure.
+func (it *CertIssuedIterator) Next() bool {
+	// If the iterator failed, stop iterating
+	if it.fail != nil {
+		return false
+	}
+	// If the iterator completed, deliver directly whatever's available
+	if it.done {
+		select {
+		case log := <-it.logs:
+			it.Event = new(CertIssued)
+			if err := it.contract.UnpackLog(it.Event, it.event, log); err != nil {
+				it.fail = err
+				return false
+			}
+			it.Event.Raw = log
+			return true
+
+		default:
+			return false
+		}
+	}
+	// Iterator still in progress, wait for either a data or an error event
+	select {
+	case log := <-it.logs:
+		it.Event = new(CertIssued)
+		if err := it.contract.UnpackLog(it.Event, it.event, log); err != nil {
+			it.fail = err
+			return false
+		}
+		it.Event.Raw = log
+		return true
+
+	case err := <-it.sub.Err():
+		it.done = true
+		it.fail = err
+		return it.Next()
+	}
+}
+
+// Error returns any retrieval or parsing error occurred during filtering.
+func (it *CertIssuedIterator) Error() error {
+	return it.fail
+}
+
+// Close terminates the iteration process, releasing any pending underlying
+// resources.
+func (it *CertIssuedIterator) Close() error {
+	it.sub.Unsubscribe()
+	return nil
+}
+
+// CertIssued represents a Issued event raised by the Cert contract.
+type CertIssued struct {
+	Course common.Hash
+	Id     *big.Int
+	Grade  string
+	Raw    types.Log // Blockchain specific contextual infos
+}
+
+// FilterIssued is a free log retrieval operation binding the contract event 0xc7fc4858662996e8fe091a9941384ab745bfc51199d7c0a5e45a791ff945993c.
+//
+// Solidity: event Issued(string indexed course, uint256 id, string grade)
+func (_Cert *CertFilterer) FilterIssued(opts *bind.FilterOpts, course []string) (*CertIssuedIterator, error) {
+
+	var courseRule []interface{}
+	for _, courseItem := range course {
+		courseRule = append(courseRule, courseItem)
+	}
+
+	logs, sub, err := _Cert.contract.FilterLogs(opts, "Issued", courseRule)
+	if err != nil {
+		return nil, err
+	}
+	return &CertIssuedIterator{contract: _Cert.contract, event: "Issued", logs: logs, sub: sub}, nil
+}
+
+// WatchIssued is a free log subscription operation binding the contract event 0xc7fc4858662996e8fe091a9941384ab745bfc51199d7c0a5e45a791ff945993c.
+//
+// Solidity: event Issued(string indexed course, uint256 id, string grade)
+func (_Cert *CertFilterer) WatchIssued(opts *bind.WatchOpts, sink chan<- *CertIssued, course []string) (event.Subscription, error) {
+
+	var courseRule []interface{}
+	for _, courseItem := range course {
+		courseRule = append(courseRule, courseItem)
+	}
+
+	logs, sub, err := _Cert.contract.WatchLogs(opts, "Issued", courseRule)
+	if err != nil {
+		return nil, err
+	}
+	return event.NewSubscription(func(quit <-chan struct{}) error {
+		defer sub.Unsubscribe()
+		for {
+			select {
+			case log := <-logs:
+				// New log arrived, parse the event and forward to the user
+				event := new(CertIssued)
+				if err := _Cert.contract.UnpackLog(event, "Issued", log); err != nil {
+					return err
+				}
+				event.Raw = log
+
+				select {
+				case sink <- event:
+				case err := <-sub.Err():
+					return err
+				case <-quit:
+					return nil
+				}
+			case err := <-sub.Err():
+				return err
+			case <-quit:
+				return nil
+			}
+		}
+	}), nil
+}
+
+// ParseIssued is a log parse operation binding the contract event 0xc7fc4858662996e8fe091a9941384ab745bfc51199d7c0a5e45a791ff945993c.
+//
+// Solidity: event Issued(string indexed course, uint256 id, string grade)
+func (_Cert *CertFilterer) ParseIssued(log types.Log) (*CertIssued, error) {
+	event := new(CertIssued)
+	if err := _Cert.contract.UnpackLog(event, "Issued", log); err != nil {
+		return nil, err
+	}
+	event.Raw = log
+	return event, nil
+}
